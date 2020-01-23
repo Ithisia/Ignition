ef07bb6d54385566f82110fabac6531e212ce112
Angela Santo <ithisia@gmail.com>
2020-01-22 21:23:42 -0800

    More Animations

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 function RenderCampsite({ campsite }) {
     return (
         <div className="col-md-5 m-1" >
-            <Card>
-                <CardImg top src={campsite.image} alt={campsite.name} />
-                <CardBody>
-                    <CardText>{campsite.description}</CardText>
-                </CardBody>
-            </Card>
+            <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            </FadeTransform>
         </div>
     );
 }
-
 
 function RenderComments({ comments }) {
     if (comments) {
@@ -29,7 +35,7 @@ function RenderComments({ comments }) {
                                     {comment.text}<br />--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
                                 </p>
                             </div>
-                            
+
                         )
                     })
                 }
@@ -59,7 +65,7 @@ function CampsiteInfo(props) {
                     <RenderComments comments={(props.comments)} />
                 </div>
             </div>
-            
+
         );
     }
     return <div />
